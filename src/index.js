f0a3f843a32cd48c30183e35bb1888470463addb
Dimmi <dima.baturo1@gmail.com>
2020-09-29 12:59:37 +0300

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+  if(array.length !==0){
+    return Math.min(...array);
+  }
+  else{
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+  if(array.length !==0){
+    return Math.max(...array);
+  }
+  else{
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+  if(array.length !==0){
+    let sum = 0;
+    array.forEach((item)=>{
+      sum = sum + item;
+    })
+    return sum/array.length;
+  }
+  else{
+    return 0;
+  }
 }
